d774e402a88c3b4d1eca3a75ada912011dbf38fb
Sanaz Movahed <s.movahed70@gmail.com>
2021-07-27 19:05:09 +0430

    add readData function

--- modules/data.py
+++ modules/data.py
@@ -1,10 +1,22 @@
-def readData(dataSetFileName,trainingPercent, validationPercent, testPercent):
-    trainingx=[1,2,3]
-    traininY=[4,5,6]
-    validationX=[7,8,9]
-    validationY=[9,8,7]
-    testx=[6,5,4]
-    testY=[3,2,1]
+def readData(dataSetFileName, validationPercent, testPercent):
 
-    return [trainingx, traininY,validationX, validationY ,testx, testY]
+    import pandas as pd   
+    data = pd.read_csv(dataSetFileName, delimiter=',')[1:]
+    
+    # Sort data in Desc order from old to new
+    data = data.iloc[::-1]
+    testPercent /= 100
+    validationPercent /= 100
+    times = sorted(data.index.values)
+    print(len(times))
+    test_percent = times[-int(testPercent * len(times))]
+    test = data[(data.index >= test_percent)]
+    
+    val_percent_before_test = times[-int((validationPercent +  testPercent) * len(times)) : -int(testPercent * len(times))]
+    validation = data[((data.index >= val_percent_before_test[0]) & (data.index <= val_percent_before_test[-1]))]
+    
+    training = data[(data.index <= val_percent_before_test[0])]
+    
+    
+    return [training, validation, test]
 
