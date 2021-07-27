d180e6588b5b99ff739d5055aa9ac14ceb0f330a
mahdi-abbaspour-shahmarasi <mahdi.abbaspour.sh@gmail.com>
2021-07-27 13:16:06 +0430

    initial commit

---
+++ modules/data.py
@@ -0,0 +1,10 @@
+def readData(dataSetFileName,trainingPercent, validationPercent, testPercent):
+    trainingx=[1,2,3]
+    traininY=[4,5,6]
+    validationX=[7,8,9]
+    validationY=[9,8,7]
+    testx=[6,5,4]
+    testY=[3,2,1]
+
+    return [trainingx, traininY,validationX, validationY ,testx, testY]
+
