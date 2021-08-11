d35335f6ee861c808239ad167cc6a3bfb3286831
sanili <s.movahed70@gmail.com>
2021-08-11 14:01:48 +0430

    solved the problem of dayOfWeek and hourOfDay in 1-hour dataset

--- modules/data.py
+++ modules/data.py
@@ -3,6 +3,7 @@ def readData(dataset_file_name, validation_percent, test_percent, FUTURE_PERIOD_
     # Import nesessary libraries
     import numpy as np
     import pandas as pd  
+    import time
     from classify import classify
     from normalization import normalize
     from sklearn import preprocessing  # for normalization of data
@@ -19,15 +20,24 @@ def readData(dataset_file_name, validation_percent, test_percent, FUTURE_PERIOD_
     data['future'] = data['close'].shift(-FUTURE_PERIOD_PREDICT)
     data['target'] = list(map(classify, data['close'], data['future']))
     # print(data[['close', 'future', 'target']].head(10))
-
+    print(data['unix'])
+    
     # Add day of week as a field
-#     data['date'] = pd.to_datetime(data['date'], infer_datetime_format=True) # , format='%Y/%m/%d-%H' 
-    data['unix'] = pd.to_datetime(data['unix'], unit="s", infer_datetime_format=True) # , errors='coerce'
+    print(data['unix'])
+    date_time = pd.Series(data['unix'])
+    temp = []
+    for num in date_time:
+        if len(str(num)) > 10:
+            b = int(num) / 1000
+        else:
+            b = int(num)
+        temp.append(time.ctime(b))
+    data['unix'] = temp
+    
+    data['unix'] = pd.to_datetime(data['unix'], format='%Y/%m/%d-%H' , infer_datetime_format=True) # , errors='coerce'
     
     data['Day of Week'] = data['unix'].dt.day_name()
     data['Hour of Day'] = data['unix'].dt.hour
-
-    print(data)
     
     # Drop the columns should not include in dataset
     data = data.drop(['unix', 'date', 'tradecount', 'symbol', 'Volume BTC', 'Volume USDT', 'future'], axis=1)
