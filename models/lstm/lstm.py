7081d2d34f6dd297b4d8b9d53301e6e444191b96
Sanaz Movahed <s.movahed70@gmail.com>
2022-11-23 12:13:35 +0330

    Update lstm.py

--- models/lstm/lstm.py
+++ models/lstm/lstm.py
@@ -4,19 +4,19 @@ def lstm(x_train):
     
     model = Sequential()
     model.add(LSTM(128, input_shape=(x_train.shape[1:])))
-#     model.add(Dropout(0.2))
-#     model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
 
-#     model.add(LSTM(128, return_sequences=True))
-#     model.add(Dropout(0.1))
-#     model.add(BatchNormalization())
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.1))
+    model.add(BatchNormalization())
 
-#     model.add(LSTM(128))
-#     model.add(Dropout(0.2))
-#     model.add(BatchNormalization())
+    model.add(LSTM(128))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
 
-#     model.add(Dense(32, activation="relu"))
-#     model.add(Dropout(0.2))
+    model.add(Dense(32, activation="relu"))
+    model.add(Dropout(0.2))
 
     model.add(Dense(1, activation="sigmoid"))
 
